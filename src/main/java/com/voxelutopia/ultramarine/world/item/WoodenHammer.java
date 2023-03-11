126d57d92200dbf3c5cfc891119f95f3497d1d5d
LocusAzzurro <dingzx1997@gmail.com>
2023-03-11 23:18:49 +0100

    :sparkles: wooden hammer shifted state toggling function
    Signed-off-by: LocusAzzurro <dingzx1997@gmail.com>

--- src/main/java/com/voxelutopia/ultramarine/world/item/WoodenHammer.java
+++ src/main/java/com/voxelutopia/ultramarine/world/item/WoodenHammer.java
@@ -1,20 +1,43 @@
 package com.voxelutopia.ultramarine.world.item;
 
 import com.voxelutopia.ultramarine.data.CreativeTabs;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
+import com.voxelutopia.ultramarine.data.ModBlockStateProperties;
+import net.minecraft.core.BlockPos;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.Optional;
 
 public class WoodenHammer extends Item {
+
     public WoodenHammer() {
-        super(new Item.Properties().tab(CreativeTabs.BUILDING_BLOCKS));
+        super(new Item.Properties().tab(CreativeTabs.BUILDING_BLOCKS).durability(100));
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        return super.use(pLevel, pPlayer, pUsedHand);
+    public InteractionResult useOn(UseOnContext pContext) {
+        Level level = pContext.getLevel();
+        ItemStack item = pContext.getItemInHand();
+        var player = Optional.ofNullable(pContext.getPlayer());
+        BlockPos blockpos = pContext.getClickedPos();
+        BlockState blockstate = level.getBlockState(blockpos);
+        if (blockstate.hasProperty(ModBlockStateProperties.SHIFTED)){
+            level.setBlock(blockpos, blockstate.setValue(ModBlockStateProperties.SHIFTED,
+                    !blockstate.getValue(ModBlockStateProperties.SHIFTED)), Block.UPDATE_ALL);
+            player.ifPresent(player1 -> {
+                item.hurtAndBreak(1, player1, p -> p.broadcastBreakEvent(pContext.getHand()));
+                player1.awardStat(Stats.ITEM_USED.get(item.getItem()));
+            });
+            return InteractionResult.sidedSuccess(level.isClientSide());
+        }
+        return super.useOn(pContext);
     }
+
+
 }
