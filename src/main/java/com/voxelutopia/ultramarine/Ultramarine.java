42306af714181db3cd417519b08b3e8a9f36a4b0
LocusAzzurro <dingzx1997@gmail.com>
2023-04-27 11:32:16 +0200

    :bento: assets for #9

--- src/main/java/com/voxelutopia/ultramarine/Ultramarine.java
+++ src/main/java/com/voxelutopia/ultramarine/Ultramarine.java
@@ -2,24 +2,13 @@ package com.voxelutopia.ultramarine;
 
 import com.mojang.logging.LogUtils;
 import com.voxelutopia.ultramarine.data.*;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.util.stream.Collectors;
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Ultramarine.MOD_ID)
 public class Ultramarine {
 
