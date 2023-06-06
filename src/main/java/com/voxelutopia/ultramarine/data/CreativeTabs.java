933999c8178a1c0350487a3dcda84c542c4ba5fb
LocusAzzurro <dingzx1997@gmail.com>
2023-06-07 00:12:43 +0200

    :recycle: make creative tabs inline with anonymous classes

--- src/main/java/com/voxelutopia/ultramarine/data/CreativeTabs.java
+++ src/main/java/com/voxelutopia/ultramarine/data/CreativeTabs.java
@@ -7,81 +7,34 @@ import net.minecraft.world.item.ItemStack;
 
 public class CreativeTabs {
 
-    public static final MaterialsTab MATERIALS = new MaterialsTab();
-    public static final ToolsTab TOOLS = new ToolsTab();
-    public static final BuildingBlocksTab BUILDING_BLOCKS = new BuildingBlocksTab();
-    public static final DecorationsTab DECORATIONS = new DecorationsTab();
+    public static final CreativeModeTab MATERIALS = new CreativeModeTab(Ultramarine.MOD_ID + ".materials") {
+        @Override public ItemStack makeIcon() {
+            return new ItemStack(ItemRegistry.CYAN_BRICK.get());
+        }
+    };
+    public static final CreativeModeTab TOOLS = new CreativeModeTab(Ultramarine.MOD_ID + ".tools") {
+        @Override public ItemStack makeIcon() {
+            return new ItemStack(ItemRegistry.WOODEN_HAMMER.get());
+        }
+    };
+    public static final CreativeModeTab BUILDING_BLOCKS = new CreativeModeTab(Ultramarine.MOD_ID + ".building_blocks") {
+        @Override public ItemStack makeIcon() {
+            return new ItemStack(ItemRegistry.CYAN_BRICKS.get());
+        }
+    };
+    public static final CreativeModeTab DECORATIONS = new CreativeModeTab(Ultramarine.MOD_ID + ".decorations") {
+        @Override public ItemStack makeIcon() {
+            return new ItemStack(ItemRegistry.BIG_BLUE_AND_WHITE_PORCELAIN_VASE.get());
+        }
+    };
     public static final CreativeModeTab FURNITURE = new CreativeModeTab(Ultramarine.MOD_ID + ".furniture") {
-        @Override
-        public ItemStack makeIcon() {
+        @Override public ItemStack makeIcon() {
             return new ItemStack(ItemRegistry.OAK_CABINET.get());
         }
     };
-    public static final LampsTab LAMPS = new LampsTab();
-
-    public static class MaterialsTab extends CreativeModeTab {
-
-        public MaterialsTab() {
-            super(Ultramarine.MOD_ID + ".materials");
-        }
-        @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ItemRegistry.CYAN_BRICK.get());
-        }
-    }
-
-    public static class ToolsTab extends CreativeModeTab {
-
-        public ToolsTab() {
-            super(Ultramarine.MOD_ID + ".tools");
-        }
-        @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ItemRegistry.WOODEN_HAMMER.get());
-        }
-    }
-
-    public static class BuildingBlocksTab extends CreativeModeTab {
-
-        public BuildingBlocksTab() {
-            super(Ultramarine.MOD_ID + ".building_blocks");
-        }
-        @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ItemRegistry.CYAN_BRICKS.get());
-        }
-    }
-
-    public static class DecorationsTab extends CreativeModeTab {
-
-        public DecorationsTab() {
-            super(Ultramarine.MOD_ID + ".decorations");
-        }
-        @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ItemRegistry.BIG_BLUE_AND_WHITE_PORCELAIN_VASE.get());
-        }
-    }
-
-    public static class FurnitureTab extends CreativeModeTab {
-
-        public FurnitureTab() {
-            super(Ultramarine.MOD_ID + ".furniture");
-        }
-        @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ItemRegistry.OAK_CABINET.get());
-        }
-    }
-
-    public static class LampsTab extends CreativeModeTab {
-
-        public LampsTab() {
-            super(Ultramarine.MOD_ID + ".lamps");
-        }
-        @Override
-        public ItemStack makeIcon() {
+    public static final CreativeModeTab LAMPS = new CreativeModeTab(Ultramarine.MOD_ID + ".lamps") {
+        @Override public ItemStack makeIcon() {
             return new ItemStack(ItemRegistry.OCTAGONAL_PALACE_LANTERN.get());
         }
-    }
+    };
 }
