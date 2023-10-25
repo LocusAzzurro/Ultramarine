2fbd53c3dbb123af17ee3980f55de4405aef17fa
LocusAzzurro <dingzx1997@gmail.com>
2023-10-25 22:24:04 +0200

    :construction: JEI woodworking recipe integration

---
+++ src/main/java/com/voxelutopia/ultramarine/client/integration/jei/UltramarinePlugin.java
@@ -0,0 +1,56 @@
+package com.voxelutopia.ultramarine.client.integration.jei;
+
+import com.voxelutopia.ultramarine.Ultramarine;
+import com.voxelutopia.ultramarine.data.recipe.WoodworkingRecipe;
+import com.voxelutopia.ultramarine.data.registry.BlockRegistry;
+import com.voxelutopia.ultramarine.data.registry.RecipeTypeRegistry;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.helpers.IJeiHelpers;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.runtime.IJeiRuntime;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import org.slf4j.Logger;
+
+import java.util.List;
+
+@JeiPlugin
+public class UltramarinePlugin implements IModPlugin {
+
+    private static final Logger LOGGER = Ultramarine.getLogger();
+
+    public UltramarinePlugin(){
+        LOGGER.info("JEI Integration Loaded");
+    }
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(Ultramarine.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        IJeiHelpers jeiHelpers = registration.getJeiHelpers();
+        IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
+        registration.addRecipeCategories(new WoodworkingRecipeCategory(guiHelper));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+
+        var recipeManager = Minecraft.getInstance().level.getRecipeManager();
+        List<WoodworkingRecipe> woodworkingRecipes = recipeManager.getAllRecipesFor(RecipeTypeRegistry.WOODWORKING.get());
+        registration.addRecipes(WoodworkingRecipeCategory.WOODWORKING_RECIPE_TYPE, woodworkingRecipes);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(BlockRegistry.WOODWORKING_WORKBENCH.get()), WoodworkingRecipeCategory.WOODWORKING_RECIPE_TYPE);
+    }
+
+}
---
+++ src/main/java/com/voxelutopia/ultramarine/client/integration/jei/WoodworkingRecipeCategory.java
@@ -0,0 +1,90 @@
+package com.voxelutopia.ultramarine.client.integration.jei;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.voxelutopia.ultramarine.Ultramarine;
+import com.voxelutopia.ultramarine.data.recipe.WoodworkingRecipe;
+import com.voxelutopia.ultramarine.data.registry.BlockRegistry;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.config.Constants;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+
+public class WoodworkingRecipeCategory implements IRecipeCategory<WoodworkingRecipe> {
+
+    public static final RecipeType<WoodworkingRecipe> WOODWORKING_RECIPE_TYPE =
+            RecipeType.create(Ultramarine.MOD_ID, "woodworking", WoodworkingRecipe.class);
+
+    public static final int WIDTH = 82;
+    public static final int HEIGHT = 34;
+
+    private final IDrawable background;
+    private final IDrawable icon;
+    private final Component localizedName;
+
+    public WoodworkingRecipeCategory(IGuiHelper guiHelper) {
+        ResourceLocation location = Constants.RECIPE_GUI_VANILLA;
+        background = guiHelper.createDrawable(location, 0, 220, WIDTH, HEIGHT);
+        icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(BlockRegistry.WOODWORKING_WORKBENCH.get()));
+        localizedName = new TranslatableComponent("gui.jei.category.woodworking");
+    }
+
+    @Override
+    public Component getTitle() {
+        return localizedName;
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return icon;
+    }
+
+    @SuppressWarnings("removal")
+    @Override
+    public ResourceLocation getUid() {
+        return new ResourceLocation(Ultramarine.MOD_ID, "woodworking");
+    }
+
+    @SuppressWarnings("removal")
+    @Override
+    public Class<? extends WoodworkingRecipe> getRecipeClass() {
+        return WoodworkingRecipe.class;
+    }
+
+    @Override
+    public RecipeType<WoodworkingRecipe> getRecipeType() {
+        return WOODWORKING_RECIPE_TYPE;
+    }
+
+    @Override
+    public boolean isHandled(WoodworkingRecipe recipe) {
+        return !recipe.isSpecial();
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, WoodworkingRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 1, 9)
+                .addIngredients(recipe.getIngredients().get(0));
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 61,  9)
+                .addItemStack(recipe.getResultItem());
+    }
+
+    @Override
+    public void draw(WoodworkingRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+        IRecipeCategory.super.draw(recipe, recipeSlotsView, stack, mouseX, mouseY);
+    }
+}
