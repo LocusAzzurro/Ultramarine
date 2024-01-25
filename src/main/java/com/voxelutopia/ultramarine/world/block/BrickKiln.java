143bd3d4493e00fe0bd6a1536526b9633cc95527
LocusAzzurro <dingzx1997@gmail.com>
2024-01-25 22:59:56 +0100

    :dizzy: brick kiln particle

--- src/main/java/com/voxelutopia/ultramarine/world/block/BrickKiln.java
+++ src/main/java/com/voxelutopia/ultramarine/world/block/BrickKiln.java
@@ -6,8 +6,11 @@ import com.voxelutopia.ultramarine.world.block.entity.BrickKilnBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -31,6 +34,8 @@ import net.minecraftforge.items.wrapper.CombinedInvWrapper;
 import net.minecraftforge.items.wrapper.RecipeWrapper;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
 
 public class BrickKiln extends DecorativeBlock implements EntityBlock, BaseBlockPropertyHolder {
 
@@ -85,6 +90,27 @@ public class BrickKiln extends DecorativeBlock implements EntityBlock, BaseBlock
             }
 
             super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+        }
+    }
+
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, Random pRand) {
+        if (pState.getValue(LIT)) {
+            double d0 = (double)pPos.getX() + 0.5D;
+            double d1 = pPos.getY();
+            double d2 = (double)pPos.getZ() + 0.5D;
+            if (pRand.nextDouble() < 0.1D) {
+                pLevel.playLocalSound(d0, d1, d2, SoundEvents.FURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0F, 1.0F, false); //todo custom sound event
+            }
+
+            Direction direction = pState.getValue(FACING);
+            Direction.Axis direction$axis = direction.getAxis();
+            double d3 = 0.52D;
+            double d4 = pRand.nextDouble() * 0.6D - 0.3D;
+            double d5 = direction$axis == Direction.Axis.X ? (double)direction.getStepX() * d3 : d4;
+            double d6 = pRand.nextDouble() * 6.0D / 16.0D;
+            double d7 = direction$axis == Direction.Axis.Z ? (double)direction.getStepZ() * d3 : d4;
+            pLevel.addParticle(ParticleTypes.SMOKE, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
+            pLevel.addParticle(ParticleTypes.FLAME, d0 + d5, d1 + d6, d2 + d7, 0.0D, 0.0D, 0.0D);
         }
     }
 
