159e48286a20970121669577adb4f6a6bb28de13
LocusAzzurro <dingzx1997@gmail.com>
2024-04-06 21:10:31 +0200

    :bento: bracket collision fix

--- src/main/java/com/voxelutopia/ultramarine/world/block/Bracket.java
+++ src/main/java/com/voxelutopia/ultramarine/world/block/Bracket.java
@@ -22,13 +22,12 @@ public class Bracket extends BaseHorizontalDirectionalBlock implements SimpleWat
 
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     private static final VoxelShape TOP = Block.box(0.0D, 10.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-    private static final VoxelShape FUNNEL = Block.box(4.0D, 4.0D, 4.0D, 12.0D, 10.0D, 12.0D);
-    private static final VoxelShape CONVEX_BASE = Shapes.or(FUNNEL, TOP);
-    private static final VoxelShape BASE = Shapes.join(CONVEX_BASE, Hopper.INSIDE, BooleanOp.ONLY_FIRST);
-    private static final VoxelShape EAST_SHAPE = Shapes.or(BASE, Block.box(12.0D, 4.0D, 6.0D, 16.0D, 8.0D, 10.0D));
-    private static final VoxelShape NORTH_SHAPE = Shapes.or(BASE, Block.box(6.0D, 4.0D, 0.0D, 10.0D, 8.0D, 4.0D));
-    private static final VoxelShape SOUTH_SHAPE = Shapes.or(BASE, Block.box(6.0D, 4.0D, 12.0D, 10.0D, 8.0D, 16.0D));
-    private static final VoxelShape WEST_SHAPE = Shapes.or(BASE, Block.box(0.0D, 4.0D, 6.0D, 4.0D, 8.0D, 10.0D));
+    private static final VoxelShape FUNNEL = Block.box(3.0D, 2.0D, 3.0D, 13.0D, 10.0D, 13.0D);
+    private static final VoxelShape BASE = Shapes.or(FUNNEL, TOP);
+    private static final VoxelShape EAST_SHAPE = Shapes.or(BASE, Block.box(12.0D, 2.0D, 3.0D, 16.0D, 8.0D, 13.0D));
+    private static final VoxelShape NORTH_SHAPE = Shapes.or(BASE, Block.box(3.0D, 2.0D, 0.0D, 13.0D, 8.0D, 4.0D));
+    private static final VoxelShape SOUTH_SHAPE = Shapes.or(BASE, Block.box(3.0D, 2.0D, 12.0D, 13.0D, 8.0D, 16.0D));
+    private static final VoxelShape WEST_SHAPE = Shapes.or(BASE, Block.box(0.0D, 2.0D, 3.0D, 4.0D, 8.0D, 13.0D));
 
     public Bracket(BaseBlockProperty property) {
         super(property);
