6f682860bc31697546e148a496c70f52415578a5
LocusAzzurro <dingzx1997@gmail.com>
2024-04-10 00:04:17 +0200

    :bento: fix terracotta pot cutout render

--- src/main/java/com/voxelutopia/ultramarine/client/event/ModClientRenderEventHandler.java
+++ src/main/java/com/voxelutopia/ultramarine/client/event/ModClientRenderEventHandler.java
@@ -56,6 +56,7 @@ public class ModClientRenderEventHandler {
             ItemBlockRenderTypes.setRenderLayer(BlockRegistry.PLATED_MUNG_BEAN_CAKES.get(), RenderType.cutout());
             ItemBlockRenderTypes.setRenderLayer(BlockRegistry.PLATED_FISH.get(), RenderType.cutout());
             ItemBlockRenderTypes.setRenderLayer(BlockRegistry.WINE_POT.get(), RenderType.cutout());
+            ItemBlockRenderTypes.setRenderLayer(BlockRegistry.TERRACOTTA_POT.get(), RenderType.cutout());
             ItemBlockRenderTypes.setRenderLayer(BlockRegistry.OCTAGONAL_PALACE_LANTERN.get(), RenderType.cutout());
             ItemBlockRenderTypes.setRenderLayer(BlockRegistry.SQUARE_PALACE_LANTERN.get(), RenderType.cutout());
             ItemBlockRenderTypes.setRenderLayer(BlockRegistry.STANDING_LAMP.get(), RenderType.cutout());
