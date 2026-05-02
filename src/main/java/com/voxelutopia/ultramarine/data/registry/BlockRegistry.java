a79a680a64cc63352bef61adc02a429df4d0d34f
Flechazo <2558755403@qq.com>
2026-05-03 03:10:06 +0800

    :bug: fix ebony bed occlusion

--- src/main/java/com/voxelutopia/ultramarine/data/registry/BlockRegistry.java
+++ src/main/java/com/voxelutopia/ultramarine/data/registry/BlockRegistry.java
@@ -770,7 +770,7 @@ public class BlockRegistry {
     public static final DeferredHolder<Block, Block> OAK_BED = register("oak_bed",
             () -> SeatDecorativeBlock.with(BaseBlockProperty.wood()).seatOffset(new Vec3(0.0f, -0.1f, 0.0f)).shaped(ShapeFunction.cardinalRotations(new RawVoxelShape(-8, 0, 0, 24, 8, 16))).directional().noOcclusion().build());
     public static final DeferredHolder<Block, Block> EBONY_BED = register("ebony_bed",
-            () -> SeatDecorativeBlock.with(BaseBlockProperty.wood()).seatOffset(new Vec3(0.0f, 0.1f, 0.0f)).shaped(ShapeFunction.cardinalRotations(new RawVoxelShape(-15, 0, -1, 31, 13, 17))).directional().build());
+            () -> SeatDecorativeBlock.with(BaseBlockProperty.wood()).seatOffset(new Vec3(0.0f, 0.1f, 0.0f)).shaped(ShapeFunction.cardinalRotations(new RawVoxelShape(-15, 0, -1, 31, 13, 17))).directional().noOcclusion().build());
 
     // SCREEN
 
